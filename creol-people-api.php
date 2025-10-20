f4db0bb0d3a00bc544b6f8e9db39accb9f064b63
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2025-10-20 18:26:40 +0000

    Adding new styling

--- creol-people-api.php
+++ creol-people-api.php
@@ -16,11 +16,16 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'CREOL_PEOPLE_API_VERSION', '1.0.0' );
 define( 'CREOL_PEOPLE_API_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CREOL_PEOPLE_API_URL', plugin_dir_url( __FILE__ ) );
+define( 'CREOL_PEOPLE_API_TRANSIENT_PREFIX', 'creol_' );
+
+// Load the API client class
+require_once plugin_dir_path( __FILE__ ) . 'src/includes/creol-people-api-client.php';
 
 // Load the shortcode class
-require_once plugin_dir_path( __FILE__ ) . 'src/includes/wp-creol-people-shortcode.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/includes/creol-people-api-shortcode.php';
+require_once plugin_dir_path( __FILE__ ) . 'src/includes/creol-alumni-api-shortcode.php';
 
 // Load the admin class
 if ( is_admin() ) {
-    require_once plugin_dir_path( __FILE__ ) . 'src/includes/wp-creol-people-admin.php';
+    require_once plugin_dir_path( __FILE__ ) . 'src/includes/creol-people-api-admin.php';
 }
